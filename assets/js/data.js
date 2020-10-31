383208f0ca06a7345ed5818c66c4520d058c2a31
Shadab Ali <shadabali604@gmail.com>
2020-10-31 21:54:13 +0530

    added: new data

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,12 +1,12 @@
-import {createUI} from './main.js'
-
+import {createUI, ByInput} from './main.js'
 let url = 'https://restcountries.eu/rest/v2/all'
 
 export let getData = async () => {
   let  data = await fetch(url);
    let jsonData = await data.json();
    console.log(jsonData)
-   createUI(jsonData)
+   createUI(jsonData);
+   ByInput(jsonData);
    let asian = jsonData.filter(data => data.region == "Asia");
    let africa = jsonData.filter(data => data.region == "Africa");
    let europe = jsonData.filter(data => data.region == "Europe");
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,18 @@
 import {print} from './helper.js'
 import {getData} from './data.js';
 
-getData();
-
 export function createUI(countries){
     let container = document.querySelector('body');
     let countryContainer = document.createElement('div');
     countryContainer.setAttribute('class', 'container country-contain');
     let countryRow = document.createElement('div');
     countryRow.setAttribute('class', 'row');
+    countryContainer.innerHTML = '';
     countries.forEach(country => {
    let countryCol = document.createElement('div');
    countryCol.setAttribute('class', 'col-md-3');
    let card = document.createElement('div');
-   card.setAttribute('class', 'card p-3 m-2');
+   card.setAttribute('class', 'card  m-2');
    let cardImg = document.createElement('img');
    cardImg.setAttribute('class', 'card-img-top');
    cardImg.src = `${country.flag}`
@@ -21,13 +20,29 @@ export function createUI(countries){
    cardBody.setAttribute('class', 'card-body');
    let cardTitle = document.createElement('h5');
    cardTitle.setAttribute('class', 'card-title');
-   cardTitle.innerText = `${country.name}`
-   card.append(cardImg, cardBody, cardTitle);
+   cardTitle.innerText = `${country.name}`;
+   let population = document.createElement('p');
+   population.innerHTML = `<span class = "font-weight-bold">Population:</span> ${country.population}`;
+   let region = document.createElement('p');
+   region.innerHTML = `<span class = "font-weight-bold">Region:</span> ${country.region}`;
+   let capital = document.createElement('p');
+   capital.innerHTML = `<span class = "font-weight-bold">Capital:</span> ${country.capital}`;
+   card.append(cardImg, cardBody, cardTitle, population, region, capital);
    countryCol.append(card);
    countryRow.appendChild(countryCol);
    countryContainer.append(countryRow);
    container.append(countryContainer);
    })
+};
+export function ByInput(countries){
+let userInput = document.querySelector('#userInput');
+userInput.addEventListener('keypress', (e) => {
+    console.log(e.target.value)
+    if(e.target.value != ''){
+        let data = countries.filter(country => country.name.includes(e.target.value));
+        createUI(data);
+     }
+})
 }
-
+getData();
 
